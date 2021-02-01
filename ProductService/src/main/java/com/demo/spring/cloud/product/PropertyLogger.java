d4b788c59cf70a3c7f1c53d37b6455ddaeec6e61
Sagar Nayak <p.sagar.nayak@gmail.com>
2021-02-02 02:55:30 +0530

    Update Property logger to log all properties during startup

--- ProductService/src/main/java/com/demo/spring/cloud/product/PropertyLogger.java
+++ ProductService/src/main/java/com/demo/spring/cloud/product/PropertyLogger.java
@@ -1,9 +1,6 @@
 package com.demo.spring.cloud.product;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
-import java.util.List;
 import java.util.stream.StreamSupport;
 
 import org.slf4j.Logger;
@@ -12,69 +9,41 @@ import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.core.env.EnumerablePropertySource;
-import org.springframework.core.env.MapPropertySource;
 import org.springframework.core.env.MutablePropertySources;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.support.StandardServletEnvironment;
 
 @Component
-public class PropertyLogger  {
+public class PropertyLogger {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(PropertyLogger.class);
+	private static final Logger LOGGER = LoggerFactory.getLogger(PropertyLogger.class);
 
-    @EventListener
-    public void handleContextRefresh(ContextRefreshedEvent event) {
-    	
-    	final ConfigurableEnvironment env = (ConfigurableEnvironment) event.getApplicationContext().getEnvironment();
-    	printApplicationProperties(env);
-    }
-    
-    private void printApplicationProperties(ConfigurableEnvironment env) {
+	@EventListener
+	public void handleContextRefresh(ContextRefreshedEvent event) {
 
-        System.out.println("************************* ACTIVE APP PROPERTIES ******************************");
+		final ConfigurableEnvironment env = (ConfigurableEnvironment) event.getApplicationContext().getEnvironment();
+		printApplicationProperties(env);
+	}
 
-        List<MapPropertySource> propertySources = new ArrayList<>();
+	private void printApplicationProperties(ConfigurableEnvironment env) {
 
-        env.getPropertySources().forEach(it -> {
-            if (it instanceof MapPropertySource && it.getName().contains("applicationConfig")) {
-                propertySources.add((MapPropertySource) it);
-            }
-        });
+		LOGGER.info("====== Environment and configuration ======");
+		LOGGER.info("Active profiles: {}", Arrays.toString(env.getActiveProfiles()));
+		if (env instanceof StandardServletEnvironment) {
+			StandardServletEnvironment sEnv = (StandardServletEnvironment) env;
 
-        propertySources.stream()
-                .map(propertySource -> propertySource.getSource().keySet())
-                .flatMap(Collection::stream)
-                .distinct()
-                .sorted()
-                .forEach(key -> {
-                    try {
-                        System.out.println(key + "=" + env.getProperty(key));
-                    } catch (Exception e) {
-                    	LOGGER.warn("{} -> {}", key, e.getMessage());
-                    }
-                });
-        System.out.println("******************************************************************************");
-    }
-    
-    private void printAllProperties(ConfigurableEnvironment env) {
+			final MutablePropertySources sources = sEnv.getPropertySources();
 
-        LOGGER.info("====== Environment and configuration ======");
-        LOGGER.info("Active profiles: {}", Arrays.toString(env.getActiveProfiles()));
-        if(env instanceof StandardServletEnvironment) {
-        	StandardServletEnvironment sEnv = (StandardServletEnvironment) env;
-        	
-        	final MutablePropertySources sources = sEnv.getPropertySources();
-        	StreamSupport.stream(sources.spliterator(), false)
-        	.filter(ps -> ps instanceof EnumerablePropertySource)
-        	.map(ps -> ((EnumerablePropertySource<?>) ps).getPropertyNames())
-        	.flatMap(Arrays::stream)
-        	.distinct()
-        	.filter(prop -> !(prop.contains("credentials") || prop.contains("password")))
-        	.sorted()
-        	.forEach(prop -> LOGGER.info("{}: {}", prop, env.getProperty(prop)));
-        }
-        LOGGER.info("===========================================");
-    
-    }
-    
+			StreamSupport.stream(sources.spliterator(), false).filter(ps -> ps instanceof EnumerablePropertySource)
+					.map(ps -> (EnumerablePropertySource<?>) ps)
+					.filter(p -> p.getName().contains("bootstrapProperties")
+							|| p.getName().contains("applicationConfig"))
+					.map(p -> p.getPropertyNames()).flatMap(Arrays::stream).distinct()
+					.filter(prop -> !(prop.contains("credentials") || prop.contains("password"))).sorted()
+					.forEach(prop -> LOGGER.info("{}: {}", prop, env.getProperty(prop)));
+		}
+		LOGGER.info("===========================================");
+
+	}
+
 }
